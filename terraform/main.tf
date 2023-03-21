7ea17726aaa1cb720cbc59137a6062ce60d5bf6f
Barbara <barbeaboo@gmail.com>
2023-03-21 06:56:34 +0100

    handling validation at jenkins server

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"] 
+  region                   = "us-east-1" 
   profile                  = "vscode"
 }
 
