cf0a63970f6eb90cd0f36e42e4fd4dabf104bb8d
Barbara <barbeaboo@gmail.com>
2023-03-21 11:07:26 +0100

    ingress

--- terraform/clusterConf.tf
+++ terraform/clusterConf.tf
@@ -10,8 +10,8 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    host                   = aws_eks_cluster.devops-cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.devops-cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
     }
 }
