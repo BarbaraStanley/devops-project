fbea49d4131c9f1572939f74d6ff951350aec461
Barbara <barbeaboo@gmail.com>
2023-03-21 23:14:15 +0100

    crowd controller 2

--- terraform/clusterConf.tf
+++ terraform/clusterConf.tf
@@ -13,11 +13,11 @@ provider "helm" {
     host                   = aws_eks_cluster.devops-cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.devops-cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
-    }
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.devops-cluster.id]
       command     = "aws"
+    }
     }
 }
 /*
